c2fb2387baf106d44b7b052e0685c381f9f20912
Mariam Machaidze <marimachaidze30@gmail.com>
2024-06-17 08:32:17 +0400

    training

--- main.py
+++ main.py
@@ -15,3 +15,17 @@ mnist = tf.keras.datasets.mnist
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 
+# neural network
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+model.add(tf.keras.layers.Dense(128, activation = 'relu'))
+model.add(tf.keras.layers.Dense(128, activation = 'relu'))
+model.add(tf.keras.layers.Dense(10, activation = 'softmax')) #output layer
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(x_train, y_train)
+
+model.save('handwritten.model')
+
